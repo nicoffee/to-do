ab1e7870cf2f7af028cb8267bee2b062e5272893
Nikolay Fedoseev <n.fedoseev@xsolla.com>
2017-07-17 15:47:17 +0500

    add Redux, tests

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore} from 'redux'
+// import { Provider } from 'react-redux'
+import './index.css'
+//import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import expect from 'expect'
+import deepFreeze from 'deep-freeze'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const toggleTodo = (todo) => {
+    return {
+        ...todo,
+        completed: !todo.completed
+    };
+};
+
+const testToggleTodo = () => {
+    const todoBefore = {
+        id: 0,
+        text: 'Learn Redux',
+        completed: false
+    };
+    const todoAfter = {
+        id: 0,
+        text: 'Learn Redux',
+        completed: true
+    };
+
+    deepFreeze(todoBefore);
+
+    expect(
+      toggleTodo(todoBefore)
+    ).toEqual(todoAfter)
+};
+
+testToggleTodo();
+
+
+console.log('All tests passed');
