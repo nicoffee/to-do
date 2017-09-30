8aec910e22040fbeec805dad8363f798a5c38853
nicoffee <fedoseevn@gmail.com>
2017-09-30 23:59:49 +0500

    fixes, styles

--- src/index.js
+++ src/index.js
@@ -1,73 +1,101 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {createStore, combineReducers} from 'redux'
-import todos from './reducers/addTodo'
-import visibilityFilter from './reducers/visibilityFilter'
-import getVisibleTodos from './reducers/getVisibleTodos'
-import {setFilter, addTodo, toggleTodo} from './actions'
-import Counter from './components/Counter'
-import './index.css'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { createStore, combineReducers } from "redux";
+import todos from "./reducers/addTodo";
+import visibilityFilter from "./reducers/visibilityFilter";
+import getVisibleTodos from "./reducers/getVisibleTodos";
+import { setFilter, addTodo, toggleTodo } from "./actions";
+import Counter from "./components/Counter";
+import "./index.css";
 
 const todoApp = combineReducers({
-    todos,
-    visibilityFilter
+  todos,
+  visibilityFilter
 });
 
 const store = createStore(todoApp);
 
-const setFilterAction = (filter) => {
-    store.dispatch(setFilter(filter))
+const setFilterAction = filter => {
+  store.dispatch(setFilter(filter));
 };
 
-class TodoApp extends React.Component {
-    render() {
-        const visibleTodos = getVisibleTodos(this.props.todos, this.props.visibilityFilter);
+class TodoApp extends Component {
+  render() {
+    const visibleTodos = getVisibleTodos(
+      this.props.todos,
+      this.props.visibilityFilter
+    );
 
-        return (
-            <div>
-              <div class="form-group">
-                <input ref={(node) => {
-                    this.input = node;
-                }}/>
-                <button onClick={() => {
-                    store.dispatch(addTodo(this.input.value));
-                    this.input.value = '';
-                }}>
-                    Add
-                </button>
-              </div>
-              
-              <ul>
-                  {visibleTodos.map((todo, id) =>
-                    <li
-                      key={id}
-                      onClick={() => store.dispatch(toggleTodo(todo.id))}
-                      style={{textDecoration: (todo.completed) ? 'line-through' : '' }}
-                    >
-                        {todo.text}
-                    </li>)
-                  }
-              </ul>
-              <span onClick={() => setFilterAction('SHOW_ALL')}>
-                  All
-              </span>
-              {' '}
-              <span onClick={() => setFilterAction('SHOW_COMPLETED')}>
-                  Completed
-              </span >
-              {' '}
-              <span onClick={() => setFilterAction('SHOW_ACTIVE')}>
-                  Active
-              </span>
-          </div>
-        )
-    }
+    const handleKeyPress = e => {
+      if (e.key === "Enter") {
+        store.dispatch(addTodo(this.input.value));
+        event.target.value = "";
+      }
+    };
+
+    return (
+      <div>
+        <div className="form-group">
+          <input
+            ref={node => {
+              this.input = node;
+            }}
+            onKeyPress={handleKeyPress}/>
+          <button
+            onClick={() => {
+              store.dispatch(addTodo(this.input.value));
+              this.input.value = "";
+            }}>
+            Add
+          </button>
+        </div>
+        <ul>
+          {visibleTodos.map((todo, id) => (
+            <li
+              key={id}
+              className={`todo ${todo.completed ? "todo--completed" : ""}`}>
+              <input
+                id={todo.id}
+                type="checkbox"
+                onClick={() => store.dispatch(toggleTodo(todo.id))}
+              />
+              <span>{todo.text}</span>
+            </li>
+          ))}
+        </ul>
+        {store.getState().visibilityFilter === "SHOW_ALL" ? (
+          <span
+            onClick={() => {
+              console.log(store.getState());
+              return setFilterAction("SHOW_ALL");
+            }}
+          >
+            All
+          </span>
+        ) : (
+          <b
+            onClick={() => {
+              console.log(store.getState());
+              return setFilterAction("SHOW_ALL");
+            }}
+          >
+            All
+          </b>
+        )}{" "}
+        <span onClick={() => setFilterAction("SHOW_COMPLETED")}>
+          Completed
+        </span>{" "}
+        <span onClick={() => setFilterAction("SHOW_ACTIVE")}>Active</span>
+      </div>
+    );
+  }
 }
 
 const render = () => {
-    ReactDOM.render(
-      <TodoApp {...store.getState()} />, document.getElementById('root')
-    );
+  ReactDOM.render(
+    <TodoApp {...store.getState()} />,
+    document.getElementById("root")
+  );
 };
 
 store.subscribe(render);
