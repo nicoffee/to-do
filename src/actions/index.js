828bea16563f177ffc41a54b864a10397104ad75
nicoffee <fedoseevn@gmail.com>
2017-09-30 12:30:14 +0500

    Add styles

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,25 +1,16 @@
 let nextToDoId = 0;
+export const addTodo = (text) => ({
+      type: 'ADD_TODO',
+      id: (nextToDoId++).toString(),
+      text,
+});
 
-export const setFilter = filter => (
-      {
-          type: 'SET_VISIBILITY_FILTER',
-          filter
-      }
-);
+export const setFilter = (filter) => ({
+      type: 'SET_VISIBILITY_FILTER',
+      filter
+});
 
-export const addTodo = text => (
-      {
-            type: 'ADD_TODO',
-            text,
-            id: nextToDoId++
-      }
-);
-
-export const toggleTodo = id => (
-      {
-            type: 'TOGGLE_TODO',
-            id: todo.id
-      }
-);
-
-
+export const toggleTodo = (id) => ({
+      type: 'TOGGLE_TODO',
+      id
+});
--- src/index.js
+++ src/index.js
@@ -5,37 +5,38 @@ import todos from './reducers/addTodo'
 import visibilityFilter from './reducers/visibilityFilter'
 import getVisibleTodos from './reducers/getVisibleTodos'
 import {setFilter, addTodo, toggleTodo} from './actions'
+import Counter from './components/Counter'
 import './index.css'
 
 const todoApp = combineReducers({
-    todos: todos,
-    visibilityFilter: visibilityFilter
+    todos,
+    visibilityFilter
 });
+
+const store = createStore(todoApp);
 
 const setFilterAction = (filter) => {
     store.dispatch(setFilter(filter))
 };
-
-const store = createStore(todoApp);
-
-let nextToDoId = 0;
 
 class TodoApp extends React.Component {
     render() {
         const visibleTodos = getVisibleTodos(this.props.todos, this.props.visibilityFilter);
 
         return (
-          <div>
-              <input ref={(node) => {
-                  this.input = node;
-              }}/>
-              <button onClick={() => {
-                  store.dispatch(addTodo(this.input.value));
-                  this.input.value = '';
-              }}
-              >
-                  Add todo
-              </button>
+            <div>
+              <div>
+                <input ref={(node) => {
+                    this.input = node;
+                }}/>
+                <button onClick={() => {
+                    store.dispatch(addTodo(this.input.value));
+                    this.input.value = '';
+                }}>
+                    Add
+                </button>
+              </div>
+              
               <ul>
                   {visibleTodos.map((todo, id) =>
                     <li
@@ -47,15 +48,15 @@ class TodoApp extends React.Component {
                     </li>)
                   }
               </ul>
-              <span onClick={setFilterAction('SHOW_ALL')}>
+              <span onClick={() => setFilterAction('SHOW_ALL')}>
                   All
               </span>
               {' '}
-              <span onClick={setFilterAction('SHOW_COMPLETED')}>
+              <span onClick={() => setFilterAction('SHOW_COMPLETED')}>
                   Completed
               </span >
               {' '}
-              <span onClick={setFilterAction('SHOW_ACTIVE')}>
+              <span onClick={() => setFilterAction('SHOW_ACTIVE')}>
                   Active
               </span>
           </div>
