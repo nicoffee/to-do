5a35aa9dfa10f51a0fb4a00fc170bde36eb8a990
nicoffee <fedoseevn@gmail.com>
2017-10-07 14:26:20 +0500

    add lifecycle hooks

--- src/containers/VisibleTodoList.jsx
+++ src/containers/VisibleTodoList.jsx
@@ -1,15 +1,45 @@
+import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import { getVisibleTodos } from "./../reducers";
 import { toggleTodo } from "./../actions";
+import { fetchTodos } from './../api';
 import TodoList from "./../components/TodoList";
 
-const mapStateToProps = (state, { match }) => ({
-  todos: getVisibleTodos(state, match.params.filter || "all")
-});
+class VisibleTodoList extends Component {
+  componentDidMount() {
+    fetchTodos(this.props.filter).then(todos => {
+      console.log(this.props.filter, todos);
+    })
+  }
 
-const VisibleTodoList = withRouter(
-  connect(mapStateToProps, { onTodoClick: toggleTodo })(TodoList)
+  componentDidUpdate(prevProps) {
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then(todos => {
+        console.log(this.props.filter, todos);
+      })
+    }
+  }
+
+  render() {
+    return <TodoList {...this.props} />;
+  }
+}
+
+const mapStateToProps = (state, { match }) => {
+  const filter = match.params.filter || "all";
+  return {
+    todos: getVisibleTodos(state, filter),
+    filter
+  }
+};
+
+VisibleTodoList = withRouter(
+  connect(mapStateToProps, { onTodoClick: toggleTodo })(VisibleTodoList)
 );
+
+// fetchTodos('all').then(todos => 
+//   console.log('todos', todos)
+// );
 
 export default VisibleTodoList;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import Root from "./components/Root"
 import configureStore from "./configureStore";
-import { fetchTodos } from './api';
 import "./index.css";
-
-fetchTodos('all').then(todos => 
-    console.log('todos', todos)
-);
 
 const store = configureStore();
 render( 
