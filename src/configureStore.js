f208ad219a8eb10b59110d0d062ba40ed67266f7
nicoffee <fedoseevn@gmail.com>
2017-10-08 21:35:42 +0500

    add wrapDispatchWithMiddlewares function

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,51 +1,48 @@
 import { createStore } from "redux";
 import todoApp from "./reducers";
 
-const addLoggingToDispatch = store => {
-  /* eslint-disable no-console */
-  const rawDispatch = store.dispatch;
-
+/* eslint-disable no-console no-undef */
+const logger = store => next => {
   if (!console.group) {
-    return rawDispatch;
+    return next;
   }
 
   return action => {
     console.group(action.type);
     console.log("%c prev state", "color: gray", store.getState());
     console.log("%c action", "color: blue", action);
-    const returnValue = rawDispatch(action);
+    const returnValue = next(action);
     console.log("%c next state", "color: green", store.getState());
     console.groupEnd(action.type);
     return returnValue;
   };
-  /* eslint-enable no-console */
+};
+/* eslint-enable no-console no-undef */
+
+const promise = store => next => action => {
+  if (typeof action.then === "function") {
+    return action.then(next);
+  }
+  return next(action);
 };
 
-const addPromiseSupportToDispatch = store => {
-  const rawDispatch = store.dispatch;
-  
-  console.log('rawDispatch', rawDispatch);
-
-  return action => {
-    if (typeof action.then === "function") {
-      return action.then(rawDispatch);
-    }
-    return rawDispatch(action);
-  };
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.forEach(
+    middleware => (store.disppatch = middleware(store)(store.dispatch))
+  );
 };
 
 const configureStore = () => {
   const store = createStore(todoApp);
+  const middlewares = [];
 
   if (process.env.NODE_ENV !== "production") { // eslint-disable-line no-undef
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logger);
   }
 
-  console.log('store.dispatch BEFORE:', store.dispatch);
+  middlewares.push(promise);
 
-  store.dispatch = addPromiseSupportToDispatch(store);
-
-  console.log('store.dispatch AFTER:', store.dispatch);
+  wrapDispatchWithMiddlewares(store, middlewares);
 
   return store;
 };
