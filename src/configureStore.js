f9373d87aa6ff4f1882ea57be86aa226a55db166
nicoffee <fedoseevn@gmail.com>
2017-10-08 21:50:30 +0500

    add middlewares from npm

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,50 +1,18 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import todoApp from "./reducers";
 
-/* eslint-disable no-console no-undef */
-const logger = store => next => {
-  if (!console.group) {
-    return next;
-  }
-
-  return action => {
-    console.group(action.type);
-    console.log("%c prev state", "color: gray", store.getState());
-    console.log("%c action", "color: blue", action);
-    const returnValue = next(action);
-    console.log("%c next state", "color: green", store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  };
-};
-/* eslint-enable no-console no-undef */
-
-const promise = store => next => action => {
-  if (typeof action.then === "function") {
-    return action.then(next);
-  }
-  return next(action);
-};
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares.forEach(
-    middleware => (store.disppatch = middleware(store)(store.dispatch))
-  );
-};
-
 const configureStore = () => {
-  const store = createStore(todoApp);
-  const middlewares = [];
-
+  const middlewares = [promise];
   if (process.env.NODE_ENV !== "production") { // eslint-disable-line no-undef
-    middlewares.push(logger);
+    middlewares.push(createLogger());
   }
 
-  middlewares.push(promise);
-
-  wrapDispatchWithMiddlewares(store, middlewares);
-
-  return store;
+  return createStore(
+    todoApp,
+    applyMiddleware(...middlewares)
+  );
 };
 
 export default configureStore;
