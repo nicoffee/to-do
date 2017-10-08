71ba889729a90cc491ab44b40286aef4a10a5923
nicoffee <fedoseevn@gmail.com>
2017-10-08 21:53:36 +0500

    fix

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import promise from 'redux-promise';
-import createLogger from 'redux-logger';
+import logger from 'redux-logger';
 import todoApp from "./reducers";
 
 const configureStore = () => {
   const middlewares = [promise];
   if (process.env.NODE_ENV !== "production") { // eslint-disable-line no-undef
-    middlewares.push(createLogger());
+    middlewares.push(logger);
   }
 
   return createStore(
