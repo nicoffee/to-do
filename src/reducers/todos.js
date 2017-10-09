f9f7a3aac6b32d43ee15427f4594fa2e905afa32
Nikolay Fedoseev <n.fedoseev@xsolla.com>
2017-10-09 11:59:01 +0500

    add filter reducers

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,20 +1,24 @@
 import { combineReducers } from "redux";
-import todo from "./todo";
 
 const byId = (state = {}, action) => {
   switch (action.type) {
-    case "ADD_TODO":
-    case "TOGGLE_TODO":
-      return {
-        ...state,
-        [action.id]: todo(state[action.id], action)
-      };
+    /* eslint-disable */
+    case "RECEIVE_TODOS":
+      const nextState = {...state};
+      action.response.forEach(todo => {
+        nextState[todo.id] = todo;
+      });
+      return nextState;
+    /* eslint-enable */
     default:
       return state;
   }
 };
 
 const allIds = (state = [], action) => {
+  if (action.filter !== 'all') {
+    return state;
+  }
   switch (action.type) {
     case "RECEIVE_TODOS":
       return action.response.map(todo => todo.id);
@@ -24,6 +28,21 @@ const allIds = (state = [], action) => {
 };
 
 const activeIds = (state = [], action) => {
+  if (action.filter !== 'active') {
+    return state;
+  }
+  switch (action.type) {
+    case "RECEIVE_TODOS":
+      return action.response.map(todo => todo.id);
+    default:
+      return state;
+  }
+};
+
+const completedIds = (state = [], action) => {
+  if (action.filter !== 'completed') {
+    return state;
+  }
   switch (action.type) {
     case "RECEIVE_TODOS":
       return action.response.map(todo => todo.id);
