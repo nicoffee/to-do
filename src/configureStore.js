9c49e17fb60ee2099c97211964460c40910cbe2b
nicoffee <fedoseevn@gmail.com>
2017-10-11 22:29:52 +0500

    add thunk implementation

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,18 +1,18 @@
 import { createStore, applyMiddleware } from "redux";
-import promise from 'redux-promise';
-import logger from 'redux-logger';
+import logger from "redux-logger";
 import todoApp from "./reducers";
 
+const thunk = store => next => action => {
+  typeof action === "function" ? action(store.dispatch) : next(action);
+};
+
 const configureStore = () => {
-  const middlewares = [promise];
+  const middlewares = [thunk];
   if (process.env.NODE_ENV !== "production") { // eslint-disable-line no-undef
     middlewares.push(logger);
   }
 
-  return createStore(
-    todoApp,
-    applyMiddleware(...middlewares)
-  );
+  return createStore(todoApp, applyMiddleware(...middlewares));
 };
 
 export default configureStore;
