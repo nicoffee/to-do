92861c1771aa82028593abb8377d9edc2bde1a7c
Nikolay Fedoseev <n.fedoseev@xsolla.com>
2017-10-12 14:51:18 +0500

    add ADD_TODO_SUCCESS reducer

--- src/reducers/byId.js
+++ src/reducers/byId.js
@@ -8,6 +8,11 @@ const byId = (state = {}, action) => {
       });
       return nextState;
     /* eslint-enable */
+    case "ADD_TODO_SUCCESS":
+      return {
+        ...state,
+        [action.response.id]: action.response
+      };
     default:
       return state;
   }
